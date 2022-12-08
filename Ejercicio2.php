232805aba8cc6faf1cd860304a0d6d14e52576e1
SamuelCalero <samuelcalero.7@gmail.com>
2022-12-08 08:23:29 -0600

    extraEjercicio2

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -31,13 +31,15 @@
 
     class AutosLujos extends Auto{
         public $cantPasajeros;
-        function __construct($numeroSerieMotor,$marca,$año,$precio,$cantPasajeros)
+        public $extra;
+        function __construct($numeroSerieMotor,$marca,$año,$precio,$cantPasajeros,$extra)
         {
             parent::__construct($numeroSerieMotor,$marca,$año,$precio);
             $this->cantPasajeros = $cantPasajeros;
+            $this->extra = $extra;
         }
         function Imprimir(){
-            print("\nEl auto es de la marca: ".$this->marca."\nEl numero de serie del motor es: ".$this->numeroSerieMotor."\nEs del año: ".$this->año."\nY su precio es de $".$this->precio."\nSu cantidad para pasajeros es: ".$this->cantPasajeros);
+            print("\nEl auto es de la marca: ".$this->marca."\nEl numero de serie del motor es: ".$this->numeroSerieMotor."\nEs del año: ".$this->año."\nY su precio es de $".$this->precio."\nSu cantidad para pasajeros es: ".$this->cantPasajeros."\nY cuenta con ".$this->extra);
         }
     }
 
@@ -62,7 +64,7 @@
     $compacto = new Compactos(5045684,"Mazda",1995,30000,2);
     $compacto->Imprimir();
     echo "\n----------------------------Lujosos----------------------------";
-    $autoLujo = new AutosLujos(54546,"Ferrari",2021,150000,2);
+    $autoLujo = new AutosLujos(54546,"Ferrari",2021,150000,2,"turbo y nitro");
     $autoLujo->Imprimir();
     echo "\n----------------------------Camionetas----------------------------";
     $camioneta = new Camionetas(6654,"BMW",2022,60000,1000,"4X4");
